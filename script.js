27f5412c7b8c6eb228b243c1447d35ef58ae180e
prarthana@example.com <prakyath@example.com>
2024-09-28 15:41:04 +0530

    added snake logic

--- script.js
+++ script.js
@@ -9,6 +9,34 @@ let snake=[{x:160,y:240},{x:140,y:240},{x:120,y:240}]
 let food={x:300,y:240} //x:15*20(cellsize) y:10*20 ,,,top left pixel of food
 
 
+function changeDirection(e)
+{
+    const isGoingdown=dy===cellSize;
+    const isGoingup=dy===-cellSize;
+    const isGoingleft=dx===-cellSize;
+    const isGoingright=dx===cellSize;
+    if(e.key==='ArrowUp' && !isGoingdown)
+    {
+        dx=0;
+        dy=-cellSize;
+    }
+    else if(e.key==='ArrowDown' && !isGoingup)
+    {
+        dx=0;
+        dy=cellSize;
+    }
+    else if(e.key==='ArrowLeft' && !isGoingright)
+    {
+        dx=-cellSize;
+        dy=0;
+    }
+    else if(e.key==='ArrowRight' && !isGoingleft)
+    {
+        dx=cellSize;
+        dy=0;   
+    }
+}
+
 function drawdiv(x,y,className)
 {
     const divelement=document.createElement('div')
@@ -33,21 +61,49 @@ function drawFoodAndSnake()
 
 }
 
+let dx=cellSize;
+let dy=0;
+
+function moveSnake()
+{
+    const newHead= {x:snake[0].x+dx,y:snake[0].y+dy};
+    snake.unshift(newHead)
+
+    if(newHead.x===food.x && newHead.y===food.y)
+    {
+        score=score+10;
+
+    }
+    else{
+        snake.pop();
+    }
+}
+
+function gameLoop()
+{
+setInterval(()=>
+{
+    moveSnake();
+    drawFoodAndSnake();
+},500)
+}
+
 function rungame()
 {
     if(!gamestarted)
     {
         gamestarted=true;
-        drawFoodAndSnake();
+        document.addEventListener('keypress',changeDirection);
+        gameLoop();
     }
 }
 
 
 function initial()
 {
-const score_board=document.createElement('div');
-score_board.id='score_board'
-document.body.insertBefore(score_board,game_arena)//at top
+// const score_board=document.createElement('div');
+// score_board.id='score_board'
+// document.body.insertBefore(score_board,game_arena)//at top
 
 const startbtn=document.createElement('btn')
 startbtn.textContent='Start Game'
